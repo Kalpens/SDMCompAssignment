dec65b16d556afccbd85cff54a627baa8d7d2fd5
Kalpens <m.kalejs@outlook.com>
2016-12-14 17:22:48 +0100

    fix

--- SDMCompAssignment/IntegerStackTest.cs
+++ SDMCompAssignment/IntegerStackTest.cs
@@ -14,7 +14,7 @@ namespace SDMCompAssignment
         [TestMethod()]
         public void AssertPush()
         {
-            IStackIntege stack = new IntegerStack();
+            IStackInteger stack = new IntegerStack();
             stack.Push(10);
             Assert.AreEqual(10, stack.Get()[0]);
         }
